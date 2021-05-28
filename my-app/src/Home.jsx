540ba049d311c4c36ae249f93506c718731863a8
MrColteR <MrColteR@mail.ru>
2021-05-28 19:02:43 +0300

    home

--- my-app/src/Home.jsx
+++ my-app/src/Home.jsx
@@ -1,9 +1,11 @@
 import React,{Component} from 'react';
+import Scrin from './schema.png';
 export class Home extends Component{
+    
     render(){
         return(
             <div className="Image mt-5 d-flex justify-content-center ">
-                <img src="./schema.png"/>
+                <img src={Scrin}/>
             </div>
         )
     }
